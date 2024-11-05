e8db355516b3f73e1a127800168cc9a0340f80e0
EC2 Default User <ec2-user@ip-172-31-9-119.ap-northeast-2.compute.internal>
2024-11-05 08:32:13 +0000

    added First Workflow Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello world from GIT");
 		System.out.println("Changed after Pushing to remote!");
+		System.out.println("Carol Workflow");
 	}
 }
 
